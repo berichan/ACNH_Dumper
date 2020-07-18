3c73a415e877c07145dbf1e6d55a59171bc5d1ea
berichan <berichandev@gmail.com>
2020-07-19 00:36:05 +0100

    foiled by endianness

--- BCSV.cs
+++ BCSV.cs
@@ -117,7 +117,7 @@ namespace ACNH_Dumper
         private ulong FiveByteInt(in int offset)
         {
             byte[] tmpBytes = new byte[8] { 0, 0, 0, 0, 0, 0, 0, 0 };
-            Array.Copy(Data.Skip(offset).Take(5).ToArray(), 0, tmpBytes, 2, 5);
+            Array.Copy(Data.Skip(offset).Take(5).ToArray(), 0, tmpBytes, 0, 5);
             return BitConverter.ToUInt64(tmpBytes, 0);
         }
 
